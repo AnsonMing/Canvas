a9d002f7211707f3c4eb5ead99af189557c59ac3
Julia Law <julialaw0701@gmail.com>
2023-12-07 13:14:01 +0800

    add drawing-random.js & drawing-randomcircle.js

---
+++ javascript/drawing-random.js
@@ -0,0 +1,8 @@
+// Add this code inside the $(document).ready() function
+$("#drawing-random").click(() => {
+    var canvas = document.getElementById("canvas-real");
+    var colors = ["red", "blue", "green", "yellow", "orange", "purple"];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    canvas.style.backgroundColor = randomColor;
+}); 
+
