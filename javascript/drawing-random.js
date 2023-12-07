958030bd8c9c597739f0cbad9f7c910198172c1c
ansonming <mingtincheung63@gmail.com>
2023-12-07 23:12:44 +0800

    updated

--- javascript/drawing-random.js
+++ javascript/drawing-random.js
@@ -1,8 +1,5 @@
 // Add this code inside the $(document).ready() function
 $("#drawing-random").click(() => {
     var canvas = document.getElementById("canvas-real");
-    var colors = ["red", "blue", "green", "yellow", "orange", "purple"];
-    var randomColor = colors[Math.floor(Math.random() * colors.length)];
-    canvas.style.backgroundColor = randomColor;
+    canvas.style.backgroundColor = "#"+Math.floor(Math.random() * 0x1000000).toString(16);
 }); 
-
