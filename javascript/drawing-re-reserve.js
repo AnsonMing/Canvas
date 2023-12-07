0846c9e916ca141962bae39413469e8ff58f2d2b
Julia Law <julialaw0701@gmail.com>
2023-12-07 16:17:59 +0800

    add redo function

--- javascript/drawing-re-reserve.js
+++ javascript/drawing-re-reserve.js
@@ -1,3 +1,19 @@
-class DrawingReReserve extends PaintFunction{
-    
-}
+// The redo functionality
+class DrawingReReserve {
+    constructor(contextReal, contextDraft) {
+      this.contextReal = contextReal;
+      this.contextDraft = contextDraft;
+    }
+  
+    // Implement the redo functionality
+    redo() {
+      // Clear the real canvas and redraw the saved canvas state
+      this.contextReal.clearRect(0, 0, canvasReal.width, canvasReal.height);
+      this.contextReal.drawImage(canvasDraft, 0, 0);
+    }
+  }
+
+  $("#drawing-re-reserve").click(() => {
+    currentFunction = new DrawingReReserve(contextReal, contextDraft);
+    currentFunction.redo();
+  });
