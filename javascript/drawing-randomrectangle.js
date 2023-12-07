0bc6871f704a46a6a662fee14d4dfdfa1dd2d18e
ansonming <mingtincheung63@gmail.com>
2023-12-07 22:50:58 +0800

    updated

--- javascript/drawing-randomrectangle.js
+++ javascript/drawing-randomrectangle.js
@@ -3,105 +3,100 @@ class DrawingRandomRectangle extends PaintFunction {
     super();
     this.contextReal = contextReal;
     this.contextDraft = contextDraft;
-    this.p1 = [];
-    this.p2 = [];
-  }
-
-  setRandomColor() {
-    this.color = getRandomColor();
-  }
-
-  setRandomPx() {
-    this.px = getRandomSize(1, 10);
+    setCustom.reset()
+    setCustom.setOption()
+    setCustom.addOption("Fill")
+    setCustom.addOption('Stroke')
   }
 
   onMouseDown(coord, event) {
-    const randomWidth = Math.random() * 100 + 50; // Random width between 50 and 150
-    const randomHeight = Math.random() * 100 + 50; // Random height between 50 and 150
+    this.color = "#"+Math.floor(Math.random() * 0x1000000).toString(16)
+    this.linewidth=Math.floor(Math.random() * 10)
+    this.randomX = Math.floor(Math.random() * 200)+10
+    this.randomY = Math.floor(Math.random() * 200)+10
+    this.p1=[coord[0],coord[1]]
+    this.p2=[coord[0]+this.randomX,coord[1]+this.randomY]
 
-    this.p1 = [coord[0], coord[1]];
-    this.p2 = [coord[0] + randomWidth, coord[1] + randomHeight];
+    this.contextDraft.beginPath();
 
-    this.setRandomColor();
-    this.setRandomPx();
-  }
+    this.contextDraft.fillStyle = this.color
+    this.contextDraft.strokeStyle = this.color
+    this.contextDraft.lineWidth = this.linewidth
+    
+    this.contextDraft.moveTo(this.p1[0],this.p1[1])
+    this.contextDraft.lineTo(this.p1[0],this.p2[1])
+    this.contextDraft.lineTo(this.p2[0],this.p2[1])
+    this.contextDraft.lineTo(this.p2[0],this.p1[1])
+    this.contextDraft.lineTo(this.p1[0],this.p1[1])
+    this.contextDraft.lineTo(this.p1[0],this.p2[1])
 
-  onMouseUp(coord, event) {
-    this.contextReal.strokeStyle = this.color;
-    this.contextReal.lineWidth = this.px;
-    this.contextReal.fillStyle = this.color;
-    this.contextReal.beginPath();
-    this.contextReal.moveTo(this.p1[0], this.p1[1]);
-    this.contextReal.lineTo(this.p1[0], this.p2[1]);
-    this.contextReal.lineTo(this.p2[0], this.p2[1]);
-    this.contextReal.lineTo(this.p2[0], this.p1[1]);
-    this.contextReal.lineTo(this.p1[0], this.p1[1]);
-    this.contextReal.lineTo(this.p1[0], this.p2[1]);
-    switch (setCustom.getOption()) {
-      case "Fill":
-        this.contextReal.fill();
-        break;
-      case "Stroke":
-        this.contextReal.stroke();
-        break;
+    switch(setCustom.getOption()){
+      case"Fill":this.contextDraft.fill();break;
+      case"Stroke":this.contextDraft.stroke();break;
     }
   }
 
   onDragging(coord, event) {
-    // Handle dragging if needed
-    const randomOffsetX = Math.random() * 20 - 10; // Random offsetX between -10 and 10
-    const randomOffsetY = Math.random() * 20 - 10; // Random offsetY between -10 and 10
+    this.contextDraft.clearRect(
+      0,
+      0,
+      canvasDraft.width,
+      canvasDraft.height,
+    )
+    this.p1=[coord[0],coord[1]]
+    this.p2=[coord[0]+this.randomX,coord[1]+this.randomY]
 
-    this.p1[0] = coord[0] + randomOffsetX;
-    this.p1[1] = coord[1] + randomOffsetY;
+    this.contextDraft.beginPath();
+    
+    this.contextDraft.moveTo(this.p1[0],this.p1[1])
+    this.contextDraft.lineTo(this.p1[0],this.p2[1])
+    this.contextDraft.lineTo(this.p2[0],this.p2[1])
+    this.contextDraft.lineTo(this.p2[0],this.p1[1])
+    this.contextDraft.lineTo(this.p1[0],this.p1[1])
+    this.contextDraft.lineTo(this.p1[0],this.p2[1])
 
-    this.p2[0] = this.p1[0] + (this.p2[0] - this.p1[0]);
-    this.p2[1] = this.p1[1] + (this.p2[1] - this.p1[1]);
-
-    this.drawRectangle();
+    switch(setCustom.getOption()){
+      case"Fill":this.contextDraft.fill();break;
+      case"Stroke":this.contextDraft.stroke();break;
+    }
   }
 
-  onMouseMove(coord, event) {
-    // Handle mouse move if needed
-    if (event.buttons !== 1) return;
+  onMouseUp(coord, event) {
+    this.contextDraft.clearRect(
+      0,
+      0,
+      canvasDraft.width,
+      canvasDraft.height,
+    )
 
-    const randomOffsetX = Math.random() * 20 - 10; // Random offsetX between -10 and 10
-    const randomOffsetY = Math.random() * 20 - 10; // Random offsetY between -10 and 10
+    this.p1=[coord[0],coord[1]]
+    this.p2=[coord[0]+this.randomX,coord[1]+this.randomY]
 
-    this.p1[0] = coord[0] + randomOffsetX;
-    this.p1[1] = coord[1] + randomOffsetY;
+    this.contextReal.beginPath();
 
-    this.p2[0] = this.p1[0] + (this.p2[0] - this.p1[0]);
-    this.p2[1] = this.p1[1] + (this.p2[1] - this.p1[1]);
+    this.contextReal.fillStyle = this.color
+    this.contextReal.strokeStyle = this.color
+    this.contextReal.lineWidth = this.linewidth
+    
+    this.contextReal.moveTo(this.p1[0],this.p1[1])
+    this.contextReal.lineTo(this.p1[0],this.p2[1])
+    this.contextReal.lineTo(this.p2[0],this.p2[1])
+    this.contextReal.lineTo(this.p2[0],this.p1[1])
+    this.contextReal.lineTo(this.p1[0],this.p1[1])
+    this.contextReal.lineTo(this.p1[0],this.p2[1])
 
-    this.drawRectangle();
+    switch(setCustom.getOption()){
+      case"Fill":this.contextReal.fill();break;
+      case"Stroke":this.contextReal.stroke();break;
+    }
+  }
+
+  onMouseMove() {
   }
 
   onMouseLeave() {
-    // Handle mouse leave if needed
-    this.clearCanvas();
   }
 
   onMouseEnter() {
-    // Handle mouse enter if needed
-  }
-
-  drawRectangle() {
-    this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-    this.contextDraft.strokeStyle = this.color;
-    this.contextDraft.lineWidth = this.px;
-    this.contextDraft.fillStyle = this.color;
-    this.contextDraft.beginPath();
-    this.contextDraft.moveTo(this.p1[0], this.p1[1]);
-    this.contextDraft.lineTo(this.p1[0], this.p2[1]);
-    this.contextDraft.lineTo(this.p2[0], this.p2[1]);
-    this.contextDraft.lineTo(this.p2[0], this.p1[1]);
-    this.contextDraft.lineTo(this.p1[0], this.p1[1]);
-    this.contextDraft.lineTo(this.p1[0], this.p2[1]);
-    this.contextDraft.stroke();
-  }
-
-  clearCanvas() {
-    this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
   }
 }
--- javascript/drawing-randomtriangle.js
+++ javascript/drawing-randomtriangle.js
@@ -1,68 +1,121 @@
 class DrawingRandomTriangle extends PaintFunction {
+
   constructor(contextReal, contextDraft) {
     super();
     this.contextReal = contextReal;
     this.contextDraft = contextDraft;
     setCustom.reset();
-    setCustom.setPx();
-    setCustom.setColor();
     setCustom.setOption();
     setCustom.addOption("Fill");
     setCustom.addOption("Stroke");
     this.p1 = [];
     this.p2 = [];
     this.p3 = [];
-    this.isFirstLine = true;
-  }
-
-  generateRandomCoordinates() {
-    const canvasWidth = canvasDraft.width;
-    const canvasHeight = canvasDraft.height;
-
-    const x1 = Math.floor(Math.random() * canvasWidth);
-    const y1 = Math.floor(Math.random() * canvasHeight);
-    const x2 = Math.floor(Math.random() * canvasWidth);
-    const y2 = Math.floor(Math.random() * canvasHeight);
-    const x3 = Math.floor(Math.random() * canvasWidth);
-    const y3 = Math.floor(Math.random() * canvasHeight);
-
-    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-
-    return [[x1, y1], [x2, y2], [x3, y3], randomColor];
   }
 
   onMouseDown(coord, event) {
-    if (this.isFirstLine) {
-      const randomCoordinates = this.generateRandomCoordinates();
-      this.p1 = randomCoordinates[0];
-      this.p2 = randomCoordinates[1];
-      this.p3 = randomCoordinates[2];
-      setCustom.setColor(randomCoordinates[3]); // Set random color
-      this.isFirstLine = false;
-    } else {
-      this.p3 = [coord[0], coord[1]];
-      this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+    
+    this.color = "#"+Math.floor(Math.random() * 0x1000000).toString(16)
+    this.linewidth=Math.floor(Math.random() * 10)
 
-      this.contextReal.beginPath();
-      this.contextReal.moveTo(this.p1[0], this.p1[1]);
-      this.contextReal.lineTo(this.p2[0], this.p2[1]);
-      this.contextReal.lineTo(this.p3[0], this.p3[1]);
-      this.contextReal.lineTo(this.p1[0], this.p1[1]);
-      this.contextReal.fillStyle = setCustom.getColor();
-      this.contextReal.strokeStyle = setCustom.getColor();
-      this.contextReal.lineWidth = setCustom.getPx();
-
-      if (setCustom.getOption() === "Fill") {
-        this.contextReal.fill();
-        this.contextReal.stroke();
-      } else {
-        this.contextReal.lineCap = "round";
-        this.contextReal.stroke();
-      }
-
-      this.isFirstLine = true;
+    this.randomInt1=Math.floor(Math.random() * 200) + 20
+    this.randomInt2=Math.floor(Math.random() * 200) + 20
+    this.randomInt3=Math.floor(Math.random() * 200) + 20
+    this.randomInt4=Math.floor(Math.random() * 200) + 20
+    if(Math.random()>0.5){
+      this.randomInt2=Math.floor(Math.random() * 200) + 20
+    }else{
+      this.randomInt2=(Math.floor(Math.random() * 200) + 20)*-1
     }
+    if(Math.random()>0.5){
+      this.randomInt4=Math.floor(Math.random() * 200) + 20
+    }else{
+      this.randomInt4=(Math.floor(Math.random() * 200) + 20)*-1
+    }
+    if(Math.random()>0.5){
+      this.randomInt1=Math.floor(Math.random() * 200) + 20
+    }else{
+      this.randomInt1=(Math.floor(Math.random() * 200) + 20)*-1
+    }
+    if(Math.random()>0.5){
+      this.randomInt3=Math.floor(Math.random() * 200) + 20
+    }else{
+      this.randomInt3=(Math.floor(Math.random() * 200) + 20)*-1
+    }
+    this.p1=[coord[0],coord[1]]
+    this.p2=[coord[0]+this.randomInt1,coord[1]+this.randomInt2]
+    this.p3=[coord[0]+this.randomInt3,coord[1]+this.randomInt4]
+    
+    this.contextDraft.fillStyle = this.color
+    this.contextDraft.strokeStyle = this.color
+    this.contextDraft.lineWidth = this.linewidth
+    this.contextDraft.lineCap = "round"
+    this.contextDraft.beginPath();
+    
+
+    this.contextDraft.moveTo(this.p1[0],this.p1[1])
+    this.contextDraft.lineTo(this.p2[0],this.p2[1])
+    this.contextDraft.lineTo(this.p3[0],this.p3[1])
+    this.contextDraft.lineTo(this.p1[0],this.p1[1])
+
+    switch(setCustom.getOption()){
+      case"Fill":this.contextDraft.fill();break;
+      case"Stroke":this.contextDraft.stroke();break;
+    }
+    
   }
 
-  // Rest of the code...
+  onDragging(coord, event) {
+    this.p1=[coord[0],coord[1]]
+    this.p2=[coord[0]+this.randomInt1,coord[1]+this.randomInt2]
+    this.p3=[coord[0]+this.randomInt3,coord[1]+this.randomInt4]
+
+    this.contextDraft.clearRect(
+      0,
+      0,
+      canvasDraft.width,
+      canvasDraft.height,
+    )
+
+    this.contextDraft.beginPath();
+    
+    this.contextDraft.moveTo(this.p1[0],this.p1[1])
+    this.contextDraft.lineTo(this.p2[0],this.p2[1])
+    this.contextDraft.lineTo(this.p3[0],this.p3[1])
+    this.contextDraft.lineTo(this.p1[0],this.p1[1])
+
+    switch(setCustom.getOption()){
+      case"Fill":this.contextDraft.fill();break;
+      case"Stroke":this.contextDraft.stroke();break;
+    }
+  }
+  onMouseMove() {}
+  onMouseUp(coord, event) {
+    this.contextDraft.clearRect(
+      0,
+      0,
+      canvasDraft.width,
+      canvasDraft.height,
+    )
+
+    this.contextReal.beginPath();
+
+    this.contextReal.fillStyle = this.color
+    this.contextReal.strokeStyle = this.color
+    this.contextReal.lineWidth = this.linewidth
+    this.contextReal.lineCap = "round"
+
+    this.contextReal.moveTo(this.p1[0],this.p1[1])
+    this.contextReal.lineTo(this.p2[0],this.p2[1])
+    this.contextReal.lineTo(this.p3[0],this.p3[1])
+    this.contextReal.lineTo(this.p1[0],this.p1[1])
+
+    switch(setCustom.getOption()){
+      case"Fill":this.contextReal.fill();break;
+      case"Stroke":this.contextReal.stroke();break;
+    }
+  }
+  onMouseLeave() {}
+  onMouseEnter() {}
+
 }
